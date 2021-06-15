ff3b8cf695287362d1f6b9129d7a40c06febc3c4
Luis-Macedo <luismacedo2001@outlook.com>
2021-06-15 10:28:45 -0300

    mudanças de design

--- salvacao/src/components/Pages/Cadastro/Cadastro.jsx
+++ salvacao/src/components/Pages/Cadastro/Cadastro.jsx
@@ -94,13 +94,35 @@ const Cadastro = () => {
                         <div className="estado">
                             <label htmlFor="estado">Escolha seu Estado:</label>
 
-                            <Field as="select" className="input2" name="estado">
+                            <Field as="select" className="input2" name="estado" required>
 
+                                <option value="AC">AC</option>
+                                <option value="AL">AL</option>
                                 <option value="AM">AM</option>
-
-                                <option value="SP">SP</option>
-
+                                <option value="AP">AP</option>
+                                <option value="BA">BA</option>
+                                <option value="CE">CE</option>
+                                <option value="DF">DF</option>
+                                <option value="ES">ES</option>
+                                <option value="GO">GO</option>
+                                <option value="MA">MA</option>
+                                <option value="MG">MG</option>
+                                <option value="MS">MS</option>
+                                <option value="MT">MT</option>
+                                <option value="PA">PA</option>
+                                <option value="PB">PB</option>
+                                <option value="PE">PE</option>
+                                <option value="PI">PI</option>
+                                <option value="PR">PR</option>
                                 <option value="RJ">RJ</option>
+                                <option value="RN">RN</option>
+                                <option value="RO">RO</option>
+                                <option value="RR">RR</option>
+                                <option value="RS">RS</option>
+                                <option value="SC">SC</option>
+                                <option value="SE">SE</option>
+                                <option value="SP">SP</option>
+                                <option value="TO">TO</option>
 
                             </Field>
 
